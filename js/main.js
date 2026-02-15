703d1fc8978245ede186117173254d5a43562118
Menachem Farkash <117067641+MenachemFarkash@users.noreply.github.com>
2026-02-15 21:41:40 +0200

    added lives logic
    Player now has different amount of lives depending on the size of the board :)

--- js/main.js
+++ js/main.js
@@ -4,219 +4,250 @@ let SIZE = 4
 let numberOfMines = 2
 const MINE = "💣"
 const COLORS = {
-  1: "#0100f8",
-  2: "#007e02",
-  3: "#fc0003",
-  4: "#00007f",
-  5: "#7c0201",
-  6: "#027e7d",
-  7: "#000000",
-  8: "#808080",
+    1: "#0100f8",
+    2: "#007e02",
+    3: "#fc0003",
+    4: "#00007f",
+    5: "#7c0201",
+    6: "#027e7d",
+    7: "#000000",
+    8: "#808080",
 }
 let isFirstClick = true
 let isGameOver = false
 let gTimer
+let bombRevealTimeout
 let gSmileyState = "playing"
 let lives = 1
 let gBoard
 
 function onInit() {
-  gBoard = createBoard(SIZE)
-  renderBoard()
-  clearInterval(gTimer)
-  minesLeftCounter()
-  changeSmiley("playing")
-  isGameOver = false
-  isFirstClick = true
+    gBoard = createBoard(SIZE)
+    renderBoard()
+    clearInterval(gTimer)
+    minesLeftCounter()
+    changeSmiley("playing")
+    updateLivesCounter()
+    isGameOver = false
+    isFirstClick = true
 }
 
 function createBoard(size) {
-  const newBoard = []
-  for (let i = 0; i < size; i++) {
-    newBoard[i] = []
-    for (let j = 0; j < size; j++) {
-      newBoard[i].push({ minesAround: 0, isRevealed: false, isMine: false, isFlagged: false, i, j })
+    const newBoard = []
+    for (let i = 0; i < size; i++) {
+        newBoard[i] = []
+        for (let j = 0; j < size; j++) {
+            newBoard[i].push({ minesAround: 0, isRevealed: false, isMine: false, isFlagged: false, i, j })
+        }
     }
-  }
 
-  return newBoard
+    return newBoard
 }
 
 function renderBoard() {
-  let boardHtml = ""
-  for (let i = 0; i < gBoard.length; i++) {
-    boardHtml += "<tr>"
-    for (let j = 0; j < gBoard[0].length; j++) {
-      boardHtml += `
+    let boardHtml = ""
+    for (let i = 0; i < gBoard.length; i++) {
+        boardHtml += "<tr>"
+        for (let j = 0; j < gBoard[0].length; j++) {
+            boardHtml += `
       <td class="cell cell-${i}-${j} covered" style="color:${COLORS[gBoard[i][j].minesAround]};" onclick="handleCellClick(this,${i},${j})" oncontextmenu="handleFlagTile(this, ${i},${j})">
       ${gBoard[i][j].isMine ? MINE : gBoard[i][j].minesAround > 0 ? gBoard[i][j].minesAround : ""}
       </td>`
+        }
+        boardHtml += "</tr>"
     }
-    boardHtml += "</tr>"
-  }
 
-  document.querySelector("tbody").innerHTML = boardHtml
+    document.querySelector("tbody").innerHTML = boardHtml
 }
 
 function placeMines(count, pos) {
-  for (let i = 0; i < count; i++) {
-    let randomTile = pickRandomTile(gBoard)
-    while (randomTile.isMine || (pos.i === randomTile.i && pos.j === randomTile.j)) {
-      randomTile = pickRandomTile(gBoard)
+    for (let i = 0; i < count; i++) {
+        let randomTile = pickRandomTile(gBoard)
+        while (randomTile.isMine || (pos.i === randomTile.i && pos.j === randomTile.j)) {
+            randomTile = pickRandomTile(gBoard)
+        }
+        gBoard[randomTile.i][randomTile.j].isMine = true
     }
-    gBoard[randomTile.i][randomTile.j].isMine = true
-  }
 }
 
 function pickRandomTile(board) {
-  let tile
-  tile = gBoard[getRandomInt(0, board.length)][getRandomInt(0, board[0].length)]
-  return tile
+    let tile
+    tile = gBoard[getRandomInt(0, board.length)][getRandomInt(0, board[0].length)]
+    return tile
 }
 
 function setupTileNumbers() {
-  for (let i = 0; i < gBoard.length; i++) {
-    for (let j = 0; j < gBoard[0].length; j++) {
-      const currentItem = gBoard[i][j]
-      if (currentItem.isMine) continue
-      currentItem.minesAround = countMinesAround({ i, j })
+    for (let i = 0; i < gBoard.length; i++) {
+        for (let j = 0; j < gBoard[0].length; j++) {
+            const currentItem = gBoard[i][j]
+            if (currentItem.isMine) continue
+            currentItem.minesAround = countMinesAround({ i, j })
+        }
     }
-  }
 }
 
 function countMinesAround(pos) {
-  let count = 0
+    let count = 0
 
-  for (var i = pos.i - 1; i <= pos.i + 1; i++) {
-    if (i < 0 || i >= gBoard.length) continue
+    for (var i = pos.i - 1; i <= pos.i + 1; i++) {
+        if (i < 0 || i >= gBoard.length) continue
 
-    for (var j = pos.j - 1; j <= pos.j + 1; j++) {
-      if (j < 0 || j >= gBoard[i].length) continue
-      if (i === pos.i && j === pos.j) continue
+        for (var j = pos.j - 1; j <= pos.j + 1; j++) {
+            if (j < 0 || j >= gBoard[i].length) continue
+            if (i === pos.i && j === pos.j) continue
 
-      if (gBoard[i][j].isMine) count++
+            if (gBoard[i][j].isMine) count++
+        }
     }
-  }
 
-  return count
+    return count
 }
 
 function handleCellClick(el, i, j) {
-  if (isFirstClick) {
-    handleFirstClick({ i, j })
+    if (isFirstClick) {
+        handleFirstClick({ i, j })
 
-    handleTimer()
-    //calling this since the el does not exist enymore
-    document.querySelector(`.cell-${i}-${j}`).classList.remove("covered")
-  }
+        handleTimer()
+        //calling this since the el does not exist enymore
+        document.querySelector(`.cell-${i}-${j}`).classList.remove("covered")
+    }
 
-  el.classList.remove("covered")
-  gBoard[i][j].isRevealed = true
-  if (gBoard[i][j].isMine) gameOver()
+    el.classList.remove("covered")
+    gBoard[i][j].isRevealed = true
+    if (gBoard[i][j].isMine) {
+        gBoard[i][j].isRevealed = true
+        bombRevealTimeout = setTimeout(() => {
+            document.querySelector(`.cell-${i}-${j}`).classList.add("covered")
+            gBoard[i][j].isRevealed = false
+        }, 1000)
+        gameOver()
+    }
 
-  revealeSafeNegTiles({ i, j })
-  checkGameWon()
+    revealeSafeNegTiles({ i, j })
+    checkGameWon()
 }
 
 function handleFirstClick(pos) {
-  placeMines(numberOfMines, { i: pos.i, j: pos.j })
-  setupTileNumbers()
-  isFirstClick = false
-  renderBoard()
+    placeMines(numberOfMines, { i: pos.i, j: pos.j })
+    setupTileNumbers()
+    isFirstClick = false
+    renderBoard()
 }
 
 function handleFlagTile(el, i, j) {
-  if (gBoard[i][j].isFlagged === true) {
-    el.classList.add("covered")
-    el.classList.remove("flagged")
-    gBoard[i][j].isFlagged = false
-    gBoard[i][j].isRevealed = false
-    minesLeftCounter()
-    // if the tile is a mine. its content now shows a mine
-    if (gBoard[i][j].isMine) {
-      el.innerText = MINE
-      return
+    if (gBoard[i][j].isFlagged === true) {
+        el.classList.add("covered")
+        el.classList.remove("flagged")
+        gBoard[i][j].isFlagged = false
+        gBoard[i][j].isRevealed = false
+        minesLeftCounter()
+        // if the tile is a mine. its content now shows a mine
+        if (gBoard[i][j].isMine) {
+            el.innerText = MINE
+            return
+        }
+
+        // Make sure the tile will not show 0 when the flag is removed than the tile is clicked :)
+        gBoard[i][j].isRevealed = true
+        el.innerText = gBoard[i][j].minesAround || ""
+        return
     }
 
-    // Make sure the tile will not show 0 when the flag is removed than the tile is clicked :)
+    el.classList.remove("covered")
+
     gBoard[i][j].isRevealed = true
-    el.innerText = gBoard[i][j].minesAround || ""
-    return
-  }
-
-  el.classList.remove("covered")
-
-  gBoard[i][j].isRevealed = true
-  gBoard[i][j].isFlagged = true
-  el.classList.add("flagged")
-  el.innerText = "🚩"
-  minesLeftCounter()
-  checkGameWon()
+    gBoard[i][j].isFlagged = true
+    el.classList.add("flagged")
+    el.innerText = "🚩"
+    minesLeftCounter()
+    checkGameWon()
 }
 
 function gameOver() {
-  for (let i = 0; i < gBoard.length; i++) {
-    for (let j = 0; j < gBoard[0].length; j++) {
-      const currentItem = gBoard[i][j]
-      if (currentItem.isMine) {
-        currentItem.isRevealed = true
-        document.querySelector(`.cell-${i}-${j}`).classList.remove("covered")
-      }
+    // checking if lives is 1 so we can perform the last ui change and lose
+    // if it was lives === 0 the player would have 4 lives
+    if (lives === 1) {
+        for (let i = 0; i < gBoard.length; i++) {
+            for (let j = 0; j < gBoard[0].length; j++) {
+                const currentItem = gBoard[i][j]
+                if (currentItem.isMine) {
+                    currentItem.isRevealed = true
+                    document.querySelector(`.cell-${i}-${j}`).classList.remove("covered")
+                }
+            }
+        }
+        isGameOver = true
+        changeSmiley("lose")
+        clearInterval(gTimer)
+        clearTimeout(bombRevealTimeout)
+        lives--
+        updateLivesCounter()
+    } else {
+        lives--
+        updateLivesCounter()
     }
-  }
-  isGameOver = true
-  changeSmiley("lose")
-  clearInterval(gTimer)
 }
 
 function checkGameWon() {
-  let completedTilesCount = 0
-  for (let i = 0; i < gBoard.length; i++) {
-    for (let j = 0; j < gBoard[0].length; j++) {
-      const currentItem = gBoard[i][j]
+    let completedTilesCount = 0
+    for (let i = 0; i < gBoard.length; i++) {
+        for (let j = 0; j < gBoard[0].length; j++) {
+            const currentItem = gBoard[i][j]
 
-      //if a mine is not flagged
-      if (currentItem.isMine && !currentItem.isFlagged) {
-        return
-      }
+            //if a mine is not flagged
+            if (currentItem.isMine && !currentItem.isFlagged) {
+                return
+            }
 
-      // if not all tiles are revealed
-      if (!currentItem.isRevealed) {
-        return
-      }
+            // if not all tiles are revealed
+            if (!currentItem.isRevealed) {
+                return
+            }
 
-      completedTilesCount++
-      if (completedTilesCount === SIZE * SIZE) {
-        clearInterval(gTimer)
-        changeSmiley("win")
-      }
+            completedTilesCount++
+            if (completedTilesCount === SIZE * SIZE) {
+                clearInterval(gTimer)
+                changeSmiley("win")
+            }
+        }
     }
-  }
 }
 
 function revealeSafeNegTiles(pos) {
-  if (gBoard[pos.i][pos.j].isMine) return
-  if (gBoard[pos.i][pos.j].minesAround === 0) {
-    for (var i = pos.i - 1; i <= pos.i + 1; i++) {
-      if (i < 0 || i >= gBoard.length) continue
+    if (gBoard[pos.i][pos.j].isMine) return
+    if (gBoard[pos.i][pos.j].minesAround === 0) {
+        for (var i = pos.i - 1; i <= pos.i + 1; i++) {
+            if (i < 0 || i >= gBoard.length) continue
 
-      for (var j = pos.j - 1; j <= pos.j + 1; j++) {
-        if (j < 0 || j >= gBoard[i].length) continue
-        if (i === pos.i && j === pos.j) continue
+            for (var j = pos.j - 1; j <= pos.j + 1; j++) {
+                if (j < 0 || j >= gBoard[i].length) continue
+                if (i === pos.i && j === pos.j) continue
 
-        if (!gBoard[i][j].isMine && !gBoard[i][j].isRevealed) {
-          const elTile = document.querySelector(`.cell-${i}-${j}`)
-          handleCellClick(elTile, i, j)
+                if (!gBoard[i][j].isMine && !gBoard[i][j].isRevealed) {
+                    const elTile = document.querySelector(`.cell-${i}-${j}`)
+                    handleCellClick(elTile, i, j)
+                }
+            }
         }
-      }
     }
-  }
 }
 
 function changeGameSize(newSize) {
-  SIZE = newSize
-  if (newSize === 4) numberOfMines = 2
-  if (newSize === 8) numberOfMines = 14
-  if (newSize === 12) numberOfMines = 32
-  onInit()
+    SIZE = newSize
+    if (newSize === 4) {
+        numberOfMines = 2
+        lives = 1
+        updateLivesCounter()
+    }
+    if (newSize === 8) {
+        numberOfMines = 14
+        lives = 2
+        updateLivesCounter()
+    }
+    if (newSize === 12) {
+        numberOfMines = 32
+        lives = 3
+        updateLivesCounter()
+    }
+    onInit()
 }
--- js/ui.js
+++ js/ui.js
@@ -1,34 +1,42 @@
 function handleTimer() {
-  const startTime = new Date()
-  let elTimer = document.querySelector(".timer")
-  gTimer = setInterval(() => {
-    let elapsedTime = Date.now() - startTime
-    elTimer.innerText = (elapsedTime / 1000).toFixed(0)
-  }, 46)
+    const startTime = new Date()
+    let elTimer = document.querySelector(".timer")
+    gTimer = setInterval(() => {
+        let elapsedTime = Date.now() - startTime
+        elTimer.innerText = (elapsedTime / 1000).toFixed(0)
+    }, 46)
 }
 
 function minesLeftCounter() {
-  let elCounter = document.querySelector(".mines-left-counter")
-  let totalFlags = 0
-  for (let i = 0; i < gBoard.length; i++) {
-    for (let j = 0; j < gBoard[0].length; j++) {
-      const currentItem = gBoard[i][j]
-      if (currentItem.isFlagged) {
-        totalFlags++
-      }
+    let elCounter = document.querySelector(".mines-left-counter")
+    let totalFlags = 0
+    for (let i = 0; i < gBoard.length; i++) {
+        for (let j = 0; j < gBoard[0].length; j++) {
+            const currentItem = gBoard[i][j]
+            if (currentItem.isFlagged) {
+                totalFlags++
+            }
+        }
     }
-  }
 
-  elCounter.innerText = numberOfMines - totalFlags
+    elCounter.innerText = numberOfMines - totalFlags
 }
 
 function changeSmiley(newState) {
-  if (newState === gSmileyState) return
-  const elSmiley = document.querySelector(".smiley-guy")
+    if (newState === gSmileyState) return
+    const elSmiley = document.querySelector(".smiley-guy")
 
-  if (newState === "playing") elSmiley.innerText = "😊"
-  if (newState === "lose") elSmiley.innerText = "🤯"
-  if (newState === "win") elSmiley.innerText = "😆"
+    if (newState === "playing") elSmiley.innerText = "😊"
+    if (newState === "lose") elSmiley.innerText = "🤯"
+    if (newState === "win") elSmiley.innerText = "😆"
 
-  gSmileyState = newState
+    gSmileyState = newState
+}
+
+function updateLivesCounter() {
+    let newLivesSTR = ""
+    for (let i = 0; i < lives; i++) {
+        newLivesSTR += "❤️"
+    }
+    document.querySelector(".lives").innerText = newLivesSTR
 }
